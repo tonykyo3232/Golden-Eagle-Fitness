7133b9f133390ba7af8d89689755c440b06715c3
jroman <jesus.roman@we2link.com>
2021-04-02 11:51:55 -0700

    added responisve Dimensions. Screen 1 ui changes

--- Screens/ScreenOne.js
+++ Screens/ScreenOne.js
@@ -3,14 +3,18 @@
 */
 import React from 'react';
 import {View, StyleSheet, SafeAreaView, Text, Button} from 'react-native';
-
+import {responsiveHeight,responsiveWidth,responsiveFontSize} from "react-native-responsive-dimensions";
 const ScreenOne = props =>{
 
     return (
         <View style={styles.container}>
-            <Text>Choose your fitness program!</Text>
+            <Text style= {styles.textStyle}>Choose your fitness program!</Text>
+            <View style= {styles.buttonContainer}>
             <Button title="Select the workout program" onPress={() => props.navigation.navigate('ScreenTwo')}/>
+            </View>
+            <View>
             <Button title="Design your own workout program" onPress={() => props.navigation.navigate('ScreenThree')}/>
+            </View>
         </View>
     );
 
@@ -22,6 +26,13 @@ const styles = StyleSheet.create({
         backgroundColor: 'teal',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    buttonContainer:{
+        marginBottom:3
+    },
+    textStyle:{
+        fontSize:responsiveFontSize(3) ,
+        fontWeight: 'bold'
     }
 });
 
